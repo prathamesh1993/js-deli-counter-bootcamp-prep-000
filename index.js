2abf11d3b9b1138cafdfbd3faf113caa47c346d7
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-02 21:30:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ function currentLine(current_line){
     for (var x in current_line){
       if (x + 1 === current_line.length){
         var last_part = x+1  + ". " + current_line[x];
-        first_part = first_part.push(last_part);
+        first_part.push(last_part);
       }else{
         var middle_part = " " +  x +1 + ". " + current_line[x] + ",";
-        first_part = first_part.push(middle_part);
+        first_part.push(middle_part);
       }
       
       
