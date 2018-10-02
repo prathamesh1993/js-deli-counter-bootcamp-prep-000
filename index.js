0eff29777359917963cfb2d06add7b1d2f261d22
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-02 20:00:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 function takeANumber(current_line,  name){
   if (current_line.length > 3){
-  return `"Welcome, ${name}. You are ${}th in the line."`
+  return `"Welcome, ${name}. You are ${}th in the line."`;
+  }
   
-  if (current_line.length > 3){
-  return `"Welcome, ${name}. You are ${}th in the line."`
+  else if (current_line.length === 3){
+  return `"Welcome, ${name}. You are ${}rd in the line."`;
+  }
+  
+  if (current_line.length === 2){
+  return `"Welcome, ${name}. You are 2nd in the line."`;
   }
 
 }
