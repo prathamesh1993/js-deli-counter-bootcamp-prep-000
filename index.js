a72011ec05f6e1820c9303e535847d7fea9930cf
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-10-02 19:59:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function takeANumber(current_line,  name){
   if (current_line.length > 3){
   return `"Welcome, ${name}. You are ${}th in the line."`
+  
+  if (current_line.length > 3){
+  return `"Welcome, ${name}. You are ${}th in the line."`
   }
 
 }
